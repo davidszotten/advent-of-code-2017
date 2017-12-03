c325c21177d25263e7000acc08f055f74ffdc072
David Szotten <davidszotten@gmail.com>
2017-12-03 18:29:40 +0000

    day 3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -1,4 +1,48 @@
 use shared::AppResult;
+use std::collections::HashMap;
+use std::ops;
+
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+impl Position {
+    fn new(x: i32, y: i32) -> Self {
+        Position{x, y}
+    }
+
+    fn neighbours(self) -> Vec<Position> {
+        let mut result = vec![];
+        for &x in [-1, 0, 1].iter() {
+            for &y in [-1, 0, 1].iter() {
+                result.push(self + Position::new(x, y));
+            }
+        }
+        result
+    }
+}
+
+impl ops::Add for Position {
+    type Output = Position;
+
+    fn add(self, other: Position) -> Position {
+        Position {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl ops::AddAssign for Position {
+    fn add_assign(&mut self, other: Position) {
+        *self = Position {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        };
+    }
+}
 
 pub fn part1(input: String) -> AppResult<u32> {
     let position: i32 = input.parse()?;
@@ -14,12 +58,12 @@ enum Direction {
     Down,
 }
 
-fn as_offset(direction: &Direction) -> (i32, i32) {
+fn as_offset(direction: &Direction) -> Position {
     match *direction {
-        Direction::Left => (-1, 0),
-        Direction::Right => (1, 0),
-        Direction::Up => (0, 1),
-        Direction::Down => (0, -1),
+        Direction::Left => Position::new(-1, 0),
+        Direction::Right => Position::new(1, 0),
+        Direction::Up => Position::new(0, 1),
+        Direction::Down => Position::new(0, -1),
     }
 }
 
@@ -39,7 +83,7 @@ struct Spiral {
     steps: i32,
     length: i32,
     direction: Direction,
-    position: (i32, i32),
+    position: Position,
 }
 
 impl Spiral {
@@ -49,19 +93,16 @@ impl Spiral {
             steps: 0,
             length: 0,
             direction: Direction::Down,
-            position: (0, 1),
+            position: Position::new(0, 1),
         }
     }
 }
 
 impl Iterator for Spiral {
-    type Item = (i32, i32);
+    type Item = Position;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.position = (
-            self.position.0 + as_offset(&self.direction).0,
-            self.position.1 + as_offset(&self.direction).1
-        );
+        self.position += as_offset(&self.direction);
 
         if self.steps == self.length {
             self.steps = 1;
@@ -73,24 +114,39 @@ impl Iterator for Spiral {
         } else {
             self.steps += 1;
         }
-        println!("{:?}", self);
+        // println!("{:?}", self);
         Some(self.position)
     }
 }
 
 
-fn find_coors(location: i32) -> (i32, i32) {
+fn find_coors(location: i32) -> Position {
     return Spiral::new().nth((location -1) as usize).expect("spiral should always have more");
 }
 
-fn distance(position: (i32, i32)) -> u32 {
-    (position.0.abs() + position.1.abs()) as u32
+fn distance(position: Position) -> u32 {
+    (position.x.abs() + position.y.abs()) as u32
 }
 
 
-pub fn part2(_input: String) -> AppResult<u32> {
+pub fn part2(input: String) -> AppResult<u32> {
+    let max: i32 = input.parse()?;
+    let mut values = HashMap::new();
+    values.insert(Position::new(0, 0), 1);
+    for position in Spiral::new() {
+        let value = position.neighbours().iter().fold(
+            0,
+            |acc, p| values.get(p).unwrap_or(&0) + acc
+        );
+        if value > max {
+            return Ok(value as u32);
+        }
+        values.insert(position, value);
+    }
     Ok(0)
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -108,20 +164,20 @@ mod tests {
     fn test_spiral() {
         println!("");
         assert_eq!(Spiral::new().take(14).collect::<Vec<_>>(), vec![
-        (0, 0),
-        (1, 0),
-        (1, 1),
-        (0, 1),
-        (-1, 1),
-        (-1, 0),
-        (-1, -1),
-        (0, -1),
-        (1, -1),
-        (2, -1),
-        (2, 0),
-        (2, 1),
-        (2, 2),
-        (1, 2),
+        Position::new(0, 0),
+        Position::new(1, 0),
+        Position::new(1, 1),
+        Position::new(0, 1),
+        Position::new(-1, 1),
+        Position::new(-1, 0),
+        Position::new(-1, -1),
+        Position::new(0, -1),
+        Position::new(1, -1),
+        Position::new(2, -1),
+        Position::new(2, 0),
+        Position::new(2, 1),
+        Position::new(2, 2),
+        Position::new(1, 2),
     ])
     }
 
