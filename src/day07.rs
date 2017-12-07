47ea6a31e5b8bed512d9eb1554be941f3d7d6ff3
David Szotten <davidszotten@gmail.com>
2017-12-07 21:17:27 +0000

    day7 part2

--- src/day07.rs
+++ src/day07.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::str::{self, FromStr};
 use nom::{IResult, alphanumeric, digit, space};
 
@@ -10,7 +10,65 @@ pub fn part1(input: &str) -> AppResult<u32> {
 }
 
 
-pub fn part2(_input: &str) -> AppResult<u32> {
+fn weight(program: &Program, program_map: &HashMap<String, Program>) -> u32 {
+    let mut sum = program.weight;
+    for child_name in program.children.clone() {
+        let child = program_map.get(&child_name).expect("child not in map");
+        sum += weight(&child, &program_map);
+    }
+    sum
+}
+
+fn balanced_children(
+    program: &Program, program_map: &HashMap<String, Program>
+) -> bool {
+
+    if program.children.len() == 0 {
+        return true;
+    }
+    let weights: HashSet<_> = program.children.iter()
+        .map(|n| weight(
+            &program_map.get(n).expect("child not in map"),
+            &program_map
+        ))
+        .collect();
+    weights.len() == 1
+}
+
+
+pub fn part2(input: &str) -> AppResult<u32> {
+    let program_map: HashMap<_,_> = input.split('\n')
+        .filter_map(|line| match program(line.as_bytes()) {
+            IResult::Done(_, p) => Some(p),
+            _ => None,
+        })
+        .map(|p| (p.name.clone(), p))
+        .collect();
+
+    for program in program_map.values() {
+        let mut weights: Vec<_> = program.children.iter()
+            .map(|n| weight(
+                &program_map.get(n).expect("child not in map"),
+                &program_map
+            ))
+            .collect();
+        if weights.len() == 0 {
+            continue;
+        }
+
+        weights.sort();
+
+        let middle = *weights.get(weights.len() / 2).expect("no middle");
+
+        for child_name in program.children.iter() {
+            let child = program_map.get(child_name).expect("child not in map");
+            let child_total_weight = weight(child, &program_map);
+            if child_total_weight != middle && balanced_children(&child, &program_map) {
+                return Ok(child.weight + middle - child_total_weight);
+                // println!("### {} {:?} {}", child.name, balanced_children(&child, &program_map), child.weight + middle - child_total_weight);
+            }
+        }
+    }
     Ok(0)
 }
 
@@ -38,12 +96,9 @@ named!(number <u32>,
 );
 
 named!(children <Vec<String>>,
-// named!(children <Vec<&str>>,
     do_parse!(
-        // tag!(" -> ") >>
         names: separated_list!(
             complete!(tag!(", ")),
-            // map_res!(complete!(alphanumeric), |s| str::from_utf8)
             do_parse!(
                 name: map_res!(complete!(alphanumeric), str::from_utf8) >>
                 (name.into())
@@ -83,7 +138,6 @@ fn bottom(input: &str) -> AppResult<String> {
         }
     }
     let bottom = seen_left.difference(&seen_right).next().expect("no bottom");
-    // !("{:?}", );
     Ok(bottom.clone())
 }
 
@@ -136,5 +190,23 @@ jptl (61)
 ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57)").unwrap(), "tknk");
+    }
+
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)").unwrap(), 60);
     }
 }
