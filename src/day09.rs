facdc17a0e89520dc046591ab017b225836bfe5d
David Szotten <davidszotten@gmail.com>
2017-12-09 12:00:27 +0000

    d9p2

--- src/day09.rs
+++ src/day09.rs
@@ -6,8 +6,8 @@ pub fn part1(input: &str) -> AppResult<u32> {
 }
 
 
-pub fn part2(_input: &str) -> AppResult<u32> {
-    Ok(0)
+pub fn part2(input: &str) -> AppResult<u32> {
+    clean_garbage(&mut input.chars(), true)
 }
 
 
@@ -54,6 +54,52 @@ fn parse_group(input: &mut impl Iterator<Item=char>, score: u32) -> AppResult<u3
 }
 
 
+fn clean_garbage(input: &mut impl Iterator<Item=char>, first: bool) -> AppResult<u32> {
+    // println!("starting");
+    let mut cleaned = 0;
+    let mut in_garbage = false;
+    let mut cancel_next = false;
+    if first {
+        assert!(input.next().expect("empty input") == '{');
+    }
+    let mut steps = 0;
+    loop {
+        steps += 1;
+        if steps > 100 {bail!("max steps exceeded")}
+        let next = input.next();
+        // println!("{}, {:?}", cleaned, next);
+        if in_garbage {
+            if cancel_next {
+                cancel_next = false;
+            } else {
+                match next {
+                    Some('>') => {in_garbage = false},
+                    Some('!') => {cancel_next = true},
+                    // Some(other) => {
+                    //     cleaned += 1;
+                    //     println!("found other in garbage: {:?}", other)
+                    // },
+                    Some(_) => {cleaned += 1},
+                    None => bail!("unclosed garbage"),
+                }
+            }
+        } else {
+            match next {
+                Some('{') => {
+                        let sub = clean_garbage(input, false)?;
+                        // println!("cleaned: {}, sub: {}", cleaned, sub);
+                        cleaned += sub;
+                    },
+                Some('}') => return Ok(cleaned),
+                Some('<') => {in_garbage = true},
+                // other => {println!("found other: {:?}", other)},
+                _ => {},
+            };
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -70,5 +116,17 @@ mod tests {
         assert_eq!(parse_group(&mut"{{<ab>},{<ab>},{<ab>},{<ab>}}".chars(), 1).unwrap(), 9);
         assert_eq!(parse_group(&mut"{{<!!>},{<!!>},{<!!>},{<!!>}}".chars(), 1).unwrap(), 9);
         assert_eq!(parse_group(&mut"{{<a!>},{<a!>},{<a!>},{<ab>}}".chars(), 1).unwrap(), 3);
+    }
+
+    #[test]
+    fn test_clean_garbage() {
+        println!("");
+        assert_eq!(clean_garbage(&mut"{<>}".chars(), true).unwrap(), 0);
+        assert_eq!(clean_garbage(&mut"{<random characters>}".chars(), true).unwrap(), 17);
+        assert_eq!(clean_garbage(&mut"{<<<<>}".chars(), true).unwrap(), 3);
+        assert_eq!(clean_garbage(&mut"{<{!>}>}".chars(), true).unwrap(), 2);
+        assert_eq!(clean_garbage(&mut"{<!!>}".chars(), true).unwrap(), 0);
+        assert_eq!(clean_garbage(&mut"{<!!!>>}".chars(), true).unwrap(), 0);
+        assert_eq!(clean_garbage(&mut"{<{o\"i!a,<{i<a>}".chars(), true).unwrap(), 10);
     }
 }
