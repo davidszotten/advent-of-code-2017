dfee825f2b32128ac5ce57c44b60b5a05380b1ee
David Szotten <davidszotten@gmail.com>
2017-12-09 10:00:59 +0000

    some more garbage handling

--- src/day09.rs
+++ src/day09.rs
@@ -22,24 +22,40 @@ fn parse_group(input: &mut impl Iterator<Item=char>, score: u32) -> AppResult<u3
     // println!("{:?}", tmp.collect::<String>());
     println!("starting with {}", score);
     let mut count = score;
+    let mut in_garbage = false;
+    let mut cancel_next = false;
     if score == 1 {
         assert!(input.next().expect("empty input") == '{');
     }
     let mut steps = 0;
     loop {
         steps += 1;
-        if steps > 10 {break Ok(10)}
+        if steps > 100 {bail!("max steps exceeded")}
         let next = input.next();
         println!("{}, {}, {:?}", score, count, next);
-        match next {
-            Some('{') => {
-                    let sub = parse_group(input, score + 1)?;
-                    println!("count: {}, sub: {}", count, sub);
-                    count += sub;
-                },
-            Some('}') => return Ok(count),
-            other => {println!("found other: {:?}", other)},
-        };
+        if in_garbage {
+            if cancel_next {
+                cancel_next = false;
+            } else {
+                match next {
+                    Some('>') => {in_garbage = false},
+                    Some('!') => {cancel_next = true},
+                    Some(other) => {println!("found other in garbage: {:?}", other)},
+                    None => bail!("unclosed garbage"),
+                }
+            }
+        } else {
+            match next {
+                Some('{') => {
+                        let sub = parse_group(input, score + 1)?;
+                        println!("count: {}, sub: {}", count, sub);
+                        count += sub;
+                    },
+                Some('}') => return Ok(count),
+                Some('<') => {in_garbage = true},
+                other => {println!("found other: {:?}", other)},
+            };
+        }
     }
 }
 
@@ -55,7 +71,10 @@ mod tests {
         // assert_eq!(parse_group(&mut"{{}}".chars(), 1).unwrap(), 3);
         // assert_eq!(parse_group(&mut"{{{}}}".chars(), 1).unwrap(), 6);
         // assert_eq!(parse_group(&mut"{{},{}}".chars(), 1).unwrap(), 5);
-        assert_eq!(parse_group(&mut"{{{},{},{{}}}}".chars(), 1).unwrap(), 16);
+        // assert_eq!(parse_group(&mut"{{{},{},{{}}}}".chars(), 1).unwrap(), 16);
         // assert_eq!(parse_group(&mut"{<a>,<a>,<a>,<a>}".chars(), 1).unwrap(), 1);
+        // assert_eq!(parse_group(&mut"{{<ab>},{<ab>},{<ab>},{<ab>}}".chars(), 1).unwrap(), 9);
+        // assert_eq!(parse_group(&mut"{{<!!>},{<!!>},{<!!>},{<!!>}}".chars(), 1).unwrap(), 9);
+        assert_eq!(parse_group(&mut"{{<a!>},{<a!>},{<a!>},{<ab>}}".chars(), 1).unwrap(), 3);
     }
 }
