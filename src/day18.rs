d0e4215d409493d9b41c3a6b215d254247b6003c
David Szotten <davidszotten@gmail.com>
2017-12-18 20:33:30 +0000

    full parsing

--- src/day18.rs
+++ src/day18.rs
@@ -1,24 +1,53 @@
 use shared::AppResult;
-use parsers::positive_integer;
+use parsers::integer;
 use nom::alpha;
+
+
+type Reg = char;
 
 
 #[derive(Debug, PartialEq)]
 enum Target {
-    Value(u32),
-    Register(char),
+    Value(i32),
+    Register(Reg),
 }
 
 #[derive(Debug, PartialEq)]
 enum Op {
     Snd(Target),
-    Set(char, Target),
+    Set(Reg, Target),
+    Add(Reg, Target),
+    Mul(Reg, Target),
+    Mod(Reg, Target),
+    Rcv(Target),
+    Jgz(Target, Target),
 }
+
+named!(parse_op <Op>,
+    alt!(
+        parse_snd |
+        parse_set |
+        parse_add |
+        parse_mul |
+        parse_mod |
+        parse_rcv |
+        parse_jgz
+    )
+);
 
 named!(parse_target <Target>,
     alt!(
-        alpha => {|r: &[u8]| Target::Register(r[0] as char)} |
-        positive_integer => {|n| Target::Value(n) }
+        alpha => {|r: &[u8]| Target::Register(r[0] as Reg)} |
+        integer => {|n| Target::Value(n) }
+    )
+);
+
+named!(parse_target_value <(Reg, Target)>,
+    do_parse!(
+        x: map!(alpha, |r: &[u8]| r[0] as Reg) >>
+        tag!(" ") >>
+        y: parse_target >>
+        ((x, y))
     )
 );
 
@@ -27,12 +56,32 @@ named!(parse_snd <Op>,
 );
 
 named!(parse_set <Op>,
+    map!(preceded!(tag!("set "), parse_target_value), |tv| Op::Set(tv.0, tv.1))
+);
+
+named!(parse_add <Op>,
+    map!(preceded!(tag!("add "), parse_target_value), |tv| Op::Add(tv.0, tv.1))
+);
+
+named!(parse_mul <Op>,
+    map!(preceded!(tag!("mul "), parse_target_value), |tv| Op::Mul(tv.0, tv.1))
+);
+
+named!(parse_mod <Op>,
+    map!(preceded!(tag!("mod "), parse_target_value), |tv| Op::Mod(tv.0, tv.1))
+);
+
+named!(parse_rcv <Op>,
+    map!(preceded!(tag!("rcv "), parse_target), |t| Op::Rcv(t))
+);
+
+named!(parse_jgz <Op>,
     do_parse!(
-        tag!("set ") >>
-        x: map!(alpha, |r: &[u8]| r[0] as char) >>
+        tag!("jgz ") >>
+        x: parse_target >>
         tag!(" ") >>
         y: parse_target >>
-        (Op::Set(x, y))
+        (Op::Jgz(x, y))
     )
 );
 
@@ -78,5 +127,40 @@ mod tests {
             parse_set(&b"set a b"[..]),
             IResult::Done(&b""[..], Op::Set('a', Target::Register('b')))
         );
+    }
+
+    #[test]
+    fn test_parser() {
+        use self::Op::*;
+        use self::Target::*;
+
+        let input = "set a 1
+add a 2
+mul a a
+mod a 5
+snd a
+set a 0
+rcv a
+jgz a -1
+set a 1
+jgz a -2";
+        let parsed: Vec<_> = input.split('\n')
+            .filter_map(|line| match parse_op(line.as_bytes()) {
+                IResult::Done(_, p) => Some(p),
+                _ => None,
+            })
+            .collect();
+        assert_eq!(parsed, vec![
+            Set('a', Value(1)),
+            Add('a', Value(2)),
+            Mul('a', Register('a')),
+            Mod('a', Value(5)),
+            Snd(Register('a')),
+            Set('a', Value(0)),
+            Rcv(Register('a')),
+            Jgz(Register('a'), Value(-1)),
+            Set('a', Value(1)),
+            Jgz(Register('a'), Value(-2)),
+        ]);
     }
 }
