9577d255fc1f0ecc005b641bbf28e55afe35b878
David Szotten <davidszotten@gmail.com>
2017-12-21 20:45:14 +0000

    d20 p2 sort of

--- src/day20.rs
+++ src/day20.rs
@@ -1,10 +1,11 @@
 use nom::IResult;
 use parsers::integer;
 use shared::AppResult;
+use std::collections::HashMap;
+use std::ops;
 
-type Triple = (i32, i32, i32);
 
-named!(parse_triple <Triple>,
+named!(parse_coor <Coor>,
     do_parse!(
         tag!("<") >>
         x: integer >>
@@ -13,21 +14,82 @@ named!(parse_triple <Triple>,
         tag!(",") >>
         z: integer >>
         tag!(">") >>
-        ((x, y, z))
+        (Coor::new(x, y, z))
     )
 );
 
-named!(parse <(Triple, Triple, Triple)>,
+named!(parse <Particle>,
     do_parse!(
         tag!("p=") >>
-        p: parse_triple >>
+        p: parse_coor >>
         tag!(", v=") >>
-        v: parse_triple >>
+        v: parse_coor >>
         tag!(", a=") >>
-        a: parse_triple >>
-        ((p, v, a))
+        a: parse_coor >>
+        (Particle::new(p, v, a))
     )
 );
+
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Particle {
+    p: Coor,
+    v: Coor,
+    a: Coor,
+}
+
+impl Particle {
+    fn new(p: Coor, v: Coor, a: Coor) -> Self {
+        Particle {p, v, a}
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Coor {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl Coor {
+    fn new(x: i32, y: i32, z: i32) -> Self {
+        Coor{x, y, z}
+    }
+
+    fn checked_add(&self, other: Coor) -> Option<Self> {
+        if let Some(x) = self.x.checked_add(other.x) {
+            if let Some(y) = self.y.checked_add(other.y) {
+                if let Some(z) = self.z.checked_add(other.z) {
+                    return Some(Coor::new(x,y,z))
+                }
+            }
+        }
+        None
+    }
+}
+
+impl ops::AddAssign for Coor {
+    fn add_assign(&mut self, rhs: Coor) {
+        *self = Coor::new(
+            self.x + rhs.x,
+            self.y + rhs.y,
+            self.z + rhs.z,
+        );
+    }
+}
+
+impl ops::Add for Coor {
+    type Output = Coor;
+    fn add(self, rhs: Coor) -> Self::Output {
+        Coor::new(
+            self.x + rhs.x,
+            self.y + rhs.y,
+            self.z + rhs.z,
+        )
+    }
+}
+
+
 
 
 pub fn part1(input: &str) -> AppResult<u32> {
@@ -40,33 +102,20 @@ pub fn part1(input: &str) -> AppResult<u32> {
     let mut largest_val = 0;
     let mut largest_pos = 0;
 
-    for (index, ((mut px, mut py, mut pz), (mut vx, mut vy, mut vz), (ax, ay, az))) in rows.into_iter().enumerate() {
+    for (index, mut particle) in rows.into_iter().enumerate() {
         // pretty hacky, rely on overflow
         let mut val = 0;
         loop {
-            vx = if let Some(res) = vx.checked_add(ax) {res}
+            particle.v = if let Some(res) = particle.v.checked_add(particle.a) {res}
             else {break};
-            px = if let Some(res) = px.checked_add(vx) {res}
+            particle.p = if let Some(res) = particle.p.checked_add(particle.v) {res}
             else {break};
-
-            vy = if let Some(res) = vy.checked_add(ay) {res}
+            let partial = if let Some(res) = particle.p.x.abs().checked_add(particle.p.y) {res}
             else {break};
-            py = if let Some(res) = py.checked_add(vy) {res}
-            else {break};
-
-            vz = if let Some(res) = vz.checked_add(az) {res}
-            else {break};
-            pz = if let Some(res) = pz.checked_add(vz) {res}
-            else {break};
-
-            let partial = if let Some(res) = px.abs().checked_add(py) {res}
-            else {break};
-            let _pos = if let Some(res) = partial.checked_add(pz.abs()) {res}
+            let _pos = if let Some(res) = partial.checked_add(particle.p.z.abs()) {res}
             else {break};
             val += 1;
         }
-
-        // println!("{}", val);
 
         if val > largest_val {
             largest_val = val;
@@ -77,9 +126,83 @@ pub fn part1(input: &str) -> AppResult<u32> {
     Ok(largest_pos as u32)
 }
 
+#[derive(Debug, PartialEq)]
+enum PState {
+    Alive(Particle),
+    Overflowed,
+    Collided,
+}
 
-pub fn part2(_input: &str) -> AppResult<u32> {
-    Ok(0)
+
+pub fn part2(input: &str) -> AppResult<u32> {
+    use self::PState::*;
+
+    let mut particles = input.split('\n')
+        .filter_map(|row| match parse(row.as_bytes()) {
+            IResult::Done(_, entry) => Some(entry),
+            o => panic!("parsing failed {:?}", o),
+        })
+        .map(|p| Alive(p))
+        .collect::<Vec<_>>();
+
+    loop {
+        let mut positions = HashMap::new();
+        println!("{:?}", particles.iter().map(|p| match p {
+            Alive(_) => Coor::new(1,0,0),
+            Overflowed => Coor::new(0,1,0),
+            Collided => Coor::new(0,0,1),
+        }).fold(Coor::new(0,0,0), |acc, x| acc + x));
+        println!("ov {}", particles.iter().filter(|p| match p {
+            Overflowed => true,
+            _ => false,
+        }).count());
+        if !particles.iter().any(|p| match p {
+            Alive(_) => true,
+            _ => false,
+        }) {
+            break;
+        }
+        // println!("{:?}", particles);
+        for position in 0..particles.len() {
+            if let Alive(mut particle) = particles[position] {
+
+                match particle.v.checked_add(particle.a) {
+                    Some(res) => {
+                        particle.v = res;
+                        match particle.p.checked_add(particle.v) {
+                            Some(res) => {
+                                particle.p = res;
+                                particles[position] = Alive(particle);
+                            },
+                            None => particles[position] = Overflowed,
+                        };
+                    },
+                    None => particles[position] = Overflowed,
+                };
+
+    // println!("{:?}", positions);
+
+                let collided_pos = positions.entry(particle.p).or_insert(position);
+                if *collided_pos != position {
+                    // println!("{:?}, {}, {}", particle.p, collided_pos, position);
+                    particles[*collided_pos] = Collided;
+                    particles[position] = Collided;
+                }
+                // if let Some(collided) = positions.get(&particle.p) {
+                //     particles[*collided] = Collided;
+                //     particles[position] = Collided;
+                // } else {
+                //     positions.insert(particle.p, position);
+                // }
+            }
+        }
+    }
+
+    Ok(particles.iter().filter(|p| match p {
+        Alive(_) => panic!("still alive?"),
+        Overflowed => true,
+        Collided => false,
+    }).count() as u32)
 }
 
 
@@ -91,7 +214,8 @@ mod tests {
     fn test_parse() {
         assert_eq!(
             parse(&b"p=<-14,1556,-678>, v=<-17,-131,-2>, a=<2,2,8>"[..]),
-            IResult::Done(&b""[..], ((-14,1556,-678), (-17, -131, -2), (2, 2, 8)))
+            IResult::Done(&b""[..], Particle::new(
+                Coor::new(-14,1556,-678), Coor::new(-17, -131, -2), Coor::new(2, 2, 8)))
         );
     }
 
@@ -99,5 +223,13 @@ mod tests {
     fn test_part1() {
         assert_eq!(part1("p=<3,0,0>, v=<2,0,0>, a=<-1,0,0>
 p=<4,0,0>, v=<0,0,0>, a=<-2,0,0>").unwrap(), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("p=<-6,0,0>, v=<3,0,0>, a=<0,0,0>
+p=<-4,0,0>, v=<2,0,0>, a=<0,0,0>
+p=<-2,0,0>, v=<1,0,0>, a=<0,0,0>
+p=<3,0,0>, v=<-1,0,0>, a=<0,0,0>").unwrap(), 1);
     }
 }
