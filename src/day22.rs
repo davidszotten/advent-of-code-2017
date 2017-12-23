c7ef25bbfca5475b354af60d1bbc1c39a6f3ec03
David Szotten <davidszotten@gmail.com>
2017-12-23 19:43:19 +0000

    d22 p2

--- src/day22.rs
+++ src/day22.rs
@@ -5,6 +5,12 @@ use direction::{Direction, Location};
 
 type Map = HashSet<Position>;
 
+struct Map2 {
+    weakened: HashSet<Position>,
+    infected:  HashSet<Position>,
+    flagged: HashSet<Position>,
+}
+
 fn parse(input: &str) -> HashSet<Position>{
     let mut map = HashSet::new();
 
@@ -17,6 +23,14 @@ fn parse(input: &str) -> HashSet<Position>{
         }
     }
     map
+}
+
+fn parse2(input: &str) -> Map2 {
+    Map2 {
+        weakened: HashSet::new(),
+        infected: parse(input),
+        flagged: HashSet::new(),
+    }
 }
 
 fn turn_left(direction: Direction) -> Direction {
@@ -38,6 +52,55 @@ fn turn_right(direction: Direction) -> Direction {
         Left => Up,
     }
 }
+
+fn walk2(mut map: Map2, steps: usize) -> usize {
+    let start = Location::new(
+        Position::new(0, 0),
+        Direction::Up
+    );
+    let mut infections = 0;
+    let mut current = start;
+
+    for _ in 0..steps {
+        if map.infected.contains(&current.position) {
+            map.infected.remove(&current.position);
+            map.flagged.insert(current.position);
+            let next_direction = turn_right(current.direction);
+            current = Location::new(
+                current.position + next_direction.as_offset(),
+                next_direction
+            );
+        }
+        else if map.weakened.contains(&current.position) {
+            map.weakened.remove(&current.position);
+            infections += 1;
+            map.infected.insert(current.position);
+            let next_direction = current.direction;
+            current = Location::new(
+                current.position + next_direction.as_offset(),
+                next_direction
+            );
+        }
+        else if map.flagged.contains(&current.position) {
+            map.flagged.remove(&current.position);
+            let next_direction = turn_left(turn_left(current.direction));
+            current = Location::new(
+                current.position + next_direction.as_offset(),
+                next_direction
+            );
+        }
+        else {
+            map.weakened.insert(current.position);
+            let next_direction = turn_left(current.direction);
+            current = Location::new(
+                current.position + next_direction.as_offset(),
+                next_direction
+            );
+        }
+    }
+    infections
+}
+
 
 fn walk(mut map: Map, steps: usize) -> usize {
     let start = Location::new(
@@ -74,8 +137,8 @@ pub fn part1(input: &str) -> AppResult<u32> {
 }
 
 
-pub fn part2(_input: &str) -> AppResult<u32> {
-    Ok(0)
+pub fn part2(input: &str) -> AppResult<u32> {
+    Ok(walk2(parse2(input), 10_000_000) as u32)
 }
 
 
@@ -103,8 +166,13 @@ mod tests {
 ..."), 10000), 5587);
     }
 
-    #[test]
-    fn test_part1() {
-        assert_eq!(part1("").unwrap(), 0);
+    #[test] #[ignore]
+    fn test_walk2() {
+        assert_eq!(walk2(parse2("..#
+#..
+..."), 100), 26);
+        assert_eq!(walk2(parse2("..#
+#..
+..."), 10000000), 2511944);
     }
 }
